a5559c3ab6ad32b023f3754f4bec57e0ec6d6430
joosting <joosting7@gmail.com>
2021-01-05 18:53:12 +0900

    매월 말일 체크

--- src/Rate.java
+++ src/Rate.java
@@ -1,6 +1,7 @@
 import java.text.DecimalFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,14 +13,14 @@ public class Rate {
 		
 		DecimalFormat formatter = new DecimalFormat("###,###");
 
-		double rate = 1.003d;
+		double rate = 1.005d;
 		
-		double amt = 70000000;
+		double amt = 2500000;
 //		double amt = 100000000;
 		
 		double profitSum = 0;
 		
-		String s = "2020-11-11";
+		String s = "2021-01-10";
 //		String e = "2028-01-04";
 		String e = "2028-01-04";
 		LocalDate start = LocalDate.parse(s);
@@ -34,38 +35,43 @@ public class Rate {
 		
 		double drawAmtSum = 0;
 		int i = 1;
-		int holiday = 0;
 		while (!start.isAfter(end)) {
 		    totalDates.add(start);
 		    start = start.plusDays(1);
-		    DayOfWeek dayOfWeek = start.getDayOfWeek();
-		    
 		    String dateStr = start.toString();
 		    
+		    YearMonth ym = YearMonth.from(start);
+		    LocalDate endDay = ym.atEndOfMonth();
+		    
+		    if(endDay.toString().equals(dateStr)) {
+		    	amt += 50000;
+		    }
+		    
+		    DayOfWeek dayOfWeek = start.getDayOfWeek();
 		    String name = dayOfWeek.name();
-			if("SATURDAY".equals(name) || "SUNDAY".equals(name) || holiday > 0) {
-				holiday--;
+			if("SATURDAY".equals(name)) {
+				System.out.println("---------------------------------------------------------------------------------------------------------------");
+		    }
+			
+			if("SATURDAY".equals(name) || "SUNDAY".equals(name)) {
+		    	continue;
+		    }
+			
+		    boolean isContinue = checkHoliday(dateStr, dayOfWeek);
+		    
+		    if(isContinue == true) {
 		    	continue;
 		    }
 		    
-			String month = dateStr.substring(0, 7);
-			if(!prevMonth.equals(month)) {
-				holiday = 2;
-				amt -= 500000;
-				prevMonth = month;
-				System.out.println("=====================================");
-			}
 			
-			
-			if(start.equals(LocalDate.parse("2021-06-30"))) {
+			if(start.equals(LocalDate.parse("2024-01-31"))) {
 				System.out.println("=====================================" + start.toString());
-				amt -= 60000000;
+				amt -= 85000000;
 			}
 			
 			if(amt > 2000000000) {
 				amt -= 200000000;
 				drawAmtSum += 200000000;
-				System.out.println("=====================drawAmtSum += 200000000");
 			}
 			
 		    double aaa = amt;
@@ -73,7 +79,8 @@ public class Rate {
 			double diff = amt- aaa;
 			profitSum += diff;
 			
-			System.out.println(i + " " + start + " => " + formatter.format(amt) +  ", " + formatter.format(diff) +  ", " + formatter.format(profitSum)  + " (" + dayOfWeek + ")");
+			String str2 = String.format("%10s %15s %15s %15s %15s %15d ", start, formatter.format(amt), formatter.format(diff), formatter.format(profitSum), dayOfWeek, i);
+			System.out.println(str2);
 			i++;
 			
 			String year = dateStr.substring(0, 4);
@@ -95,6 +102,43 @@ public class Rate {
 		
 		System.out.println("drawAmtSum => " + formatter.format(drawAmtSum));
 				
+	}
+
+	private static boolean checkHoliday(String dateStr, DayOfWeek dayOfWeek) {
+		boolean isContinue = false;
+		if(dateStr.endsWith("01-01") || dateStr.endsWith("03-01") || dateStr.endsWith("05-05") || dateStr.endsWith("06-06") || dateStr.endsWith("08-15") || dateStr.endsWith("10-03") || dateStr.endsWith("10-09") || dateStr.endsWith("12-25") || dateStr.endsWith("12-31")) {
+			isContinue = true;
+		}
+		
+		if(dateStr.equals("2021-02-11") || dateStr.equals("2021-02-12") || dateStr.equals("2021-05-19")
+				|| dateStr.equals("2021-09-20") || dateStr.equals("2021-09-21") || dateStr.equals("2021-09-22")) {
+			isContinue = true;
+		}
+		
+		if(dateStr.equals("2022-01-31") || dateStr.equals("2022-02-01") || dateStr.equals("2022-02-02") || dateStr.equals("2022-03-09")
+				|| dateStr.equals("2022-06-01") || dateStr.equals("2022-09-09") || dateStr.equals("2022-09-12")) {
+			isContinue = true;
+		}
+		
+		if(dateStr.equals("2023-01-23") || dateStr.equals("2023-09-28") || dateStr.equals("2023-09-29")) {
+			isContinue = true;
+		}
+		
+		if(dateStr.equals("2024-02-09") || dateStr.equals("2024-04-10") || dateStr.equals("2024-05-15") || dateStr.equals("2024-09-16") || dateStr.equals("2024-09-17") || dateStr.equals("2024-09-18")) {
+			isContinue = true;
+		}
+		
+		if(dateStr.equals("2025-01-28") || dateStr.equals("2025-01-29") || dateStr.equals("2025-01-30")
+				|| dateStr.equals("2025-10-06") || dateStr.equals("2025-10-06")) {
+			isContinue = true;
+		}
+		
+		if(isContinue == true) {
+			String str2 = String.format("%10s %63s %15s", dateStr, dayOfWeek, "HOLIDAY");
+			System.out.println(str2);
+		}
+		
+		return isContinue;
 	}
 
 }
